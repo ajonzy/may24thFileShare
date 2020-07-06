ed85fa9437e0aa0713ff3f2997eeec30a160a75f
Alex Jones <ajonzy@gmail.com>
2020-07-06 13:17:35 -0600

    Added signup functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import FileShare from "./pages/fileShare/fileShare"
+import Auth from "./pages/login/auth"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <FileShare />
+        <Auth />
       </div>
     );
   }
