a27b1ffede16c2305cd14000d5735982888522b9
Alex Jones <ajonzy@gmail.com>
2020-07-07 16:57:09 -0600

    Added error management to login and signup components

--- src/components/pages/login/auth.js
+++ src/components/pages/login/auth.js
@@ -10,7 +10,9 @@ export default class Auth extends Component {
        this.state = {
            authMethod: "login",
            usernameInput: "",
-           passwordInput: ""
+           passwordInput: "",
+           passwordConfirmInput: "",
+           errorMessage: "none"
        }
 
        this.handleClick = this.handleClick.bind(this)
@@ -20,32 +22,86 @@ export default class Auth extends Component {
    }
 
    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value })
+        this.setState({ 
+            [event.target.name]: event.target.value,
+            errorMessage: "none"
+        })
     }
 
     handleSignup(event) {
         event.preventDefault()
-        
-        fetch("http://127.0.0.1:5000/user/create", {
-            method: "POST",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify({
-                username: this.state.usernameInput,
-                password: this.state.passwordInput
+
+        if (this.state.usernameInput === "" || this.state.passwordInput === "" || this.state.passwordConfirmInput === "") {
+            this.setState({ errorMessage: "blank field" })
+        }
+        else if (this.state.passwordInput !== this.state.passwordConfirmInput) {
+            this.setState({ errorMessage: "mismatched passwords" })
+        }
+        else {
+            fetch("http://127.0.0.1:5000/user/create", {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify({
+                    username: this.state.usernameInput,
+                    password: this.state.passwordInput
+                })
             })
-        })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+
+                if (data === "Username Taken") {
+                    this.setState({ errorMessage: "username taken" })
+                }
+                else {
+                    this.setState({ errorMessage: "none" })
+                }
+            })
+            .catch(error => {
+                console.log(error)
+                this.setState({ errorMessage: "fetch error" })
+            })
+        }
    }
 
    handleLogin(event) {
        event.preventDefault()
-       console.log("Login!")
+
+       if (this.state.usernameInput === "" || this.state.passwordInput === "") {
+           this.setState({ errorMessage: "blank field" })
+       }
+       else {
+           fetch("http://127.0.0.1:5000/user/verification", {
+               method: "POST",
+               headers: { "content-type": "application/json" },
+               body: JSON.stringify({
+                   username: this.state.usernameInput,
+                   password: this.state.passwordInput
+               })
+           })
+           .then(response => response.json())
+           .then(data => {
+               console.log(data)
+
+               if (data === "User NOT Verified") {
+                   this.setState({ errorMessage: "not verified" })
+               }
+               else {
+                   this.setState({ errorMessage: "none" })
+               }
+            })
+           .catch(error => {
+               console.log(error)
+               this.setState({ errorMessage: "fetch error" })
+            })
+       }
    }
 
    handleClick() {
-       this.setState({ authMethod: "signup" })
+       this.setState({ 
+           authMethod: this.state.authMethod === "login" ? "signup" : "login",
+           errorMessage: "none"
+        })
    }
 
    render() {
@@ -57,14 +113,19 @@ export default class Auth extends Component {
                         handleSubmit={this.handleLogin}
                         usernameInput={this.state.usernameInput}
                         passwordInput={this.state.passwordInput}
+                        errorMessage={this.state.errorMessage}
+                        handleClick={this.handleClick}
                     /> 
                     : <Signup 
                         handleChange={this.handleChange} 
                         handleSubmit={this.handleSignup}
                         usernameInput={this.state.usernameInput}
                         passwordInput={this.state.passwordInput}
+                        passwordConfirmInput={this.state.passwordConfirmInput}
+                        errorMessage={this.state.errorMessage}
+                        handleClick={this.handleClick}
                     />}
-               <p onClick={this.handleClick}>Don't have an account? Click here to sign up!</p>
+               
            </div>
        )
    }
--- src/components/pages/login/login.js
+++ src/components/pages/login/login.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export default function login(props) {
+    const errorMessage = {
+        "none": "",
+        "blank field": "Please fill in all fields.",
+        "fetch error": "An error occured. Please try again later.",
+        "not verified": "Incorrect username or password."
+    }
+
     return (
         <form onSubmit={props.handleSubmit}>
             <input 
@@ -18,6 +25,8 @@ export default function login(props) {
                     onChange={props.handleChange}
                 />
             <button>Login</button>
+            <p className="error">{errorMessage[props.errorMessage]}</p>
+            <p onClick={props.handleClick}>Don't have an account? Click here to sign up!</p>
         </form>
     )
 }
